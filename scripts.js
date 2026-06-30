9c65f6419d116fe923a9fd13daad928d2db0ca5a
Anderson Gomes <andersongmsolv@gmail.com>
2026-06-29 23:06:40 -0300

    added currency

--- scripts.js
+++ scripts.js
@@ -1,7 +1,49 @@
-const amount = document.getElementById ("amount")
+const USD = 5.19
+const EUR = 5.92
+const GBP = 6.87
 
-//Manipulando o input para receber apenas números
+const form = document.querySelector ("form")
+const amount = document.getElementById ("amount")
+const currency = document.getElementById ("currency")
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
+
 amount.addEventListener("input", () => {
   const hasCharactersRegex = /\D+/g
   amount.value = amount.value.replace(hasCharactersRegex, "")
 })
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$")
+      break
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£")
+  }
+}
+
+function convertCurrency(amount, price, symbol) {
+  try {
+    description.textContent = `${symbol}1 = ${formatCurrencyBRL(price)}`
+    
+    footer.classList.add("show-result")
+
+  } catch (error) {
+    console.log(error)
+    footer.classList.remove("show-result")
+    alert("Não foi possível converter, tente novamente mais tarde")
+  }
+}
+
+function formatCurrencyBRL(value){
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+})
+}
